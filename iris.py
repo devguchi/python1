c24b23f9ef9f9bb52784d3096a59983d25022cc4
devguchi <devguchitaro@gmail.com>
2021-09-16 20:30:47 +0900

    added df.dropna

--- iris.py
+++ iris.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('iris.csv')
-print(df.head(3))
+print(df.tail(3))
+
+print(df['種類'].unique())
+print(df['種類'].value_counts())
+print(df.isnull().any(axis=0))
+print(df.sum())
+print(df.isnull().sum())
+df2 = df.dropna(how='any', axis=0)
+print(df2.tail(3))

5804505050afdb8b5b11b5611e31451bdd720fcb
devguchi <devguchitaro@gmail.com>
2021-09-16 22:27:54 +0900

    fix

--- iris.py
+++ iris.py
@@ -5,11 +5,9 @@ from sklearn import tree
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('iris.csv')
-colmean = df.mean()
-df = df.fillna(colmean)
-xcol = ['がく片長さ','がく片幅','花弁長さ','花弁幅']
-x = df[xcol]
-t = df['種類']
+df = df.fillna(df.mean())
+x = df[['g_len','g_width','k_len','k_width']]
+t = df['type']
 x_train,x_test,y_train,y_test = train_test_split(
         x,t,test_size=0.3,random_state=0)
 
@@ -22,15 +20,8 @@ print(model.score(x_test,y_test))
 with open('iris.pkl', 'wb') as f:
     pickle.dump(model, f)
 
-print(model.tree_.feature)
-print(model.tree_.threshold)
-print(model.tree_.value[1])
-print(model.tree_.value[3])
-print(model.tree_.value[4])
-print(model.classes_)
-
-x_train.columns = ['glen','gwith','klen','kwidth']
 plt.figure(figsize=(15,10))
 tree.plot_tree(
         model,feature_names=x_train.columns,filled=True)
 plt.show()
+

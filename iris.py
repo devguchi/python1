01be170f606d4a8dce29a2962c6e35c24ac9ee92
devguchi <devguchitaro@gmail.com>
2021-09-16 21:18:31 +0900

    holdout

--- iris.py
+++ iris.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('iris.csv')
 colmean = df.mean()
@@ -7,9 +8,11 @@ df = df.fillna(colmean)
 xcol = ['がく片長さ','がく片幅','花弁長さ','花弁幅']
 x = df[xcol]
 t = df['種類']
+x_train,x_test,y_train,y_test = train_test_split(
+        x,t,test_size=0.3,random_state=0)
 
 model = tree.DecisionTreeClassifier(
         max_depth=2, random_state=0)
 
-model.fit(x,t)
-print(model.score(x,t))
+model.fit(x_train,y_train)
+print(model.score(x_test,y_test))

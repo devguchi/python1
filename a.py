aaee4291aa652893951e99ed85ecad579c424a5f
devguchi <devguchitaro@gmail.com>
2021-09-16 18:12:49 +0900

    first

---
+++ a.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn import tree
+
+df = pd.read_csv('data.csv')
+x = df[['age','score']]
+t = df['money']
+
+model = tree.DecisionTreeClassifier(random_state = 0)
+model.fit(x, t)
+
+a = [34,55]
+b = [12,60]
+c = [25,30]
+d = [30,70]
+print(model.predict([a,b,c,d]))
